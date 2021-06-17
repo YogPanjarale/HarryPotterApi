298c17fe7de218bbda1da3da91a146187398df41
YogPanjarale <yog.panjarale@gmail.com>
2021-06-17 11:44:48 +0530

    get all characters  and by id

--- src/dynamo.ts
+++ src/dynamo.ts
@@ -1,7 +1,5 @@
 import AWS from "aws-sdk";
-import dotenv from 'dotenv';
 import { Character } from "./types";
-dotenv.config()
 
 AWS.config.update({
     region:process.env.AWS_DEFAULT_REGION,
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,35 @@
-console.log("Hello World 🚀");
-// import { getCharacters, addOrUpdateCharacter } from "./dynamo";
-// import {seedData} from './seed';
-// seedData()
+import dotenv from 'dotenv';
+dotenv.config()
+import express from 'express'
+import { getCharacterById, getCharacters } from './dynamo';
+
+const app = express()
+
+app.get('/',(_req,res)=>{
+    res.send("Hello World!")
+})
+app.get('/characters',async(_req,res)=>{
+    try{
+        const characters = await getCharacters()
+        res.json(characters)
+    }catch(error){
+        console.error(error);
+        res.status(500).json({"error":"Some thing went wrong :("})
+    }
+})
+app.get('/characters/:id',async(req,res)=>{
+    const id =req.params.id
+    try{
+        const character = await getCharacterById(id)
+        res.json(character)
+    }catch(error){
+        console.error(error);
+        res.status(500).json({"error":"Some thing went wrong :("})
+    }
+})
+const port = process.env.PORT || 3000;
+
+app.listen(port,()=>{
+    console.log(`Express App listening on http://localhost:${port}`);
+    
+})
